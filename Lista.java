d7a46c27071a1ee3d6873738eea24babbe4ff4cc
Carlos Daniel Umasi Cariapaza <cumasic@unsa.edu.pe>
2022-05-16 16:20:08 -0500

    Cree una clase lista donde iran algunos datos basicos para una lista

---
+++ Lista.java
@@ -0,0 +1,10 @@
+public class Lista {
+    private String nombre;
+	private int edad;
+	private int salario;
+	public Lista(String n,int e,int s) {
+		nombre = n;
+		edad = e;
+		salario = s;
+	}
+}
